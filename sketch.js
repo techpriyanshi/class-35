746e2a9ba0e1c8c6e2a503620319a3919a6bc932
bunny <82957061+techpriyanshi@users.noreply.github.com>
2021-07-16 14:53:08 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,8 +4,8 @@ var database;
 var foodS,foodStock;
 
 function preload(){
-   dogImg=loadImage("images/dogImg.png");
-   dogImg1=loadImage("images/dogImg1.png");
+   dogImg=loadImage("Dog.png");
+   dogImg1=loadImage("Happy.png");
   }
 
 //Function to set initial environment
